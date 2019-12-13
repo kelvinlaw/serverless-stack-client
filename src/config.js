f12557533d7bcb6f10ce7e31081f7a558efdd30e
Kelvin Law <kelvinlaw@gmail.com>
2019-12-12 22:14:03 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-kelvinator"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://p4169367c7.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_7NQK8a8eF",
+      APP_CLIENT_ID: "29j7sb2g7shurnprj9udvja3uo",
+      IDENTITY_POOL_ID: "us-east-2_7NQK8a8eF"
+    }
+};
